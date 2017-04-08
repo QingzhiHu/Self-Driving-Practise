97164f93b1d141fbd258cc9d7c0ea980947b82a7
QingzhiHu <1552794394@qq.com>
2017-04-08 16:34:44 +0800

    wrote contents

---
+++ Tensorflow/solution.py
@@ -0,0 +1,38 @@
+# Quiz Solution
+# Note: You can't run code in this tab
+import tensorflow as tf
+
+
+def run():
+    output = None
+    x = tf.placeholder(tf.int32)
+
+    with tf.Session() as sess:
+        output = sess.run(x, feed_dict={x: 123})
+
+    return output
+
+
+# TODO: Convert the following to TensorFlow:
+x = tf.constant(10)
+y = tf.constant(2)
+z = tf.subtract(tf.divide(x,y),tf.cast(tf.constant(1), tf.float64))
+
+# TODO: Print z from a session
+with tf.Session() as sess:
+    output = sess.run(z)
+    print(output)
+
+
+
+softmax_data = [0.7, 0.2, 0.1]
+one_hot_data = [1.0, 0.0, 0.0]
+
+softmax = tf.placeholder(tf.float32)
+one_hot = tf.placeholder(tf.float32)
+
+# ToDo: Print cross entropy from session
+cross_entropy = -tf.reduce_sum(tf.multiply(one_hot, tf.log(softmax)))
+
+with tf.Session() as sess:
+    print(sess.run(cross_entropy, feed_dict={softmax: softmax_data, one_hot: one_hot_data}))
